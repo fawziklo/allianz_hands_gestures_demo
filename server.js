b4306b452436d69ffd3d70bdeb396cd4307401ed
R.Fawzi <fawzi.rida@hotmail.com>
2021-09-13 00:09:06 +0200

    add express js

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+
 const app = express();
 
-// Serve static files....
-app.use(express.static(__dirname + '/dist/ng-tfjs-demo'));
+app.use(express.static("./dist/ng-tfjs-demo"));
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/ng-tfjs-demo/index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", { root: "dist/ng-tfjs-demo" });
 });
 
-// default Heroku PORT
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`);
