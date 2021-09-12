5bfc43525e9af7b4070e2ff141835850989d63c2
R.Fawzi <fawzi.rida@hotmail.com>
2021-09-12 21:47:10 +0200

    delete sentiment folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,20 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  // {
-  //   path: 'home',
-  //   loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
-  // },
-  // {
-  //   path: 'about',
-  //   loadChildren: () =>
-  //     import('./about/about.module').then((m) => m.AboutModule),
-  // },
-  {
-    path: 'sentiment',
-    loadChildren: () =>
-      import('./sentiment-analysis/sentiment-analysis.module').then((m) => m.SentimentAnalysisModule),
-  },
+   {
+    path: 'home',
+     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+   },
+   {
+    path: 'about',
+     loadChildren: () =>
+      import('./about/about.module').then((m) => m.AboutModule),
+   },
   {
     path: '',
     pathMatch: 'full',
